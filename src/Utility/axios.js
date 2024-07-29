2050c4a30a56627adf1b3aab4d098232807e1caf
Abdulwasiee <wasungncom@gmail.com>
2024-07-29 17:45:47 +0300

    updated

--- src/Utility/axios.js
+++ src/Utility/axios.js
@@ -1,9 +1,9 @@
-// Utility/axios.js
 import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "https://amazon-api-deploy-e0ng.onrender.com",
+  baseURL:'http://localhost:4146',
+  // baseURL: "https://amazon-api-deploy-e0ng.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
